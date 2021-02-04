4da2b84e90681bfae6223cf186cf648238d77076
dstearle <dallas.st.earle@gmail.com>
2021-02-04 09:47:15 -0800

    Created scraper for the locations page to retrieve all of the location titles

--- scrape2.js
+++ scrape2.js
@@ -3,13 +3,25 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 // The page to be scraped
-request('https://americangrilledcheesecompany.com/', (error, response, html) => {
+request('https://americangrilledcheesecompany.com/locations/', (error, response, html) => {
 
     // Scrape the page if there are no errors
     if(!error && response.statusCode == 200) {
 
         // The html to be scraped from the page
         const $ = cheerio.load(html);
+
+        // For each loop that goes through all of the location titles
+        $('.locations-list').each((i, el) => {
+
+            const title = $(el)
+            .find('h3')
+            .text()
+            .replace(/\s\s+/g, '');
+
+            console.log(title);
+
+        });
 
     }
 
